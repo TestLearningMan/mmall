d0f8e20e793097a91928bca5898219d2774eef5e
chunxi_lei <q7213536@yeah.net>
2019-03-16 09:38:44 +0800

    最新

--- src/main/java/com/mmall/common/ResponseCode.java
+++ src/main/java/com/mmall/common/ResponseCode.java
@@ -1,23 +1,26 @@
 package com.mmall.common;
 
 public enum ResponseCode {
+
     SUCCESS(0,"SUCCESS"),
     ERROR(1,"ERROR"),
-    NEED_LOGIN(2,"NEED LOGIN"),
-    ILLEGAL(3,"ILLEGAL_ARGUMENT");
+    NEED_LOGIN(10,"NEED_LOGIN"),
+    ILLEGAL_ARGUMENT(2,"ILLEGAL_ARGUMENT");
 
     private final int code;
-    private final String msg;
-     ResponseCode(int code,String msg){
-        this.code=code;
-        this.msg=msg;
+    private final String desc;
+
+
+    ResponseCode(int code,String desc){
+        this.code = code;
+        this.desc = desc;
     }
 
-    public int getCode() {
+    public int getCode(){
         return code;
     }
-
-    public String getMsg() {
-        return msg;
+    public String getDesc(){
+        return desc;
     }
+
 }
